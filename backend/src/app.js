dc5bf3cc4820a8847f709ab07ce2bcea86a6b671
umarkhan905 <umairzahidkhang@gmail.com>
2024-08-29 05:32:14 +0500

    Create express js server

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+export { app };
