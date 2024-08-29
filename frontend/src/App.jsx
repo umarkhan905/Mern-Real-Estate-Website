a6338c942b7b52838e07db95cafb86b28beed75f
umarkhan905 <umairzahidkhang@gmail.com>
2024-08-29 05:26:09 +0500

    Installing react-router-dom package and create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { About, Home, Profile, SignIn, SignUp } from "./pages";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import About from "./About";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+
+export { Home, Profile, About, SignUp, SignIn };
