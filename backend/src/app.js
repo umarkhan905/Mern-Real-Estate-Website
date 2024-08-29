38994c890d3db601c602cce824d1f75226ff2dbb
umarkhan905 <umairzahidkhang@gmail.com>
2024-08-29 06:16:29 +0500

    setting user routes and controllers

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,4 +6,9 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+// import routes
+import userRoute from "./routes/user.route.js";
+
+app.use("/api/v1", userRoute);
+
 export { app };
