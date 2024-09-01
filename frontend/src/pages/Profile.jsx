ff9a37b02b362462fef67010e71194da1d962a56
umarkhan905 <umairzahidkhang@gmail.com>
2024-09-01 10:29:34 +0500

    complete show user listings functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -33,6 +33,7 @@ const Profile = () => {
     avatar: currentUser.avatar,
     password: "",
   });
+  const [userListings, setUserListings] = useState([]);
   const fileRef = useRef();
   const dispatch = useDispatch();
 
@@ -139,6 +140,19 @@ const Profile = () => {
       toast.error(error.message);
     }
   };
+
+  const handleShowListings = async () => {
+    try {
+      const res = await fetch("/api/v1/listings");
+      const data = await res.json();
+      setUserListings(data.data);
+    } catch (error) {
+      console.log("Error in profile page", error);
+    }
+  };
+
+  const handleListingDelete = async (id) => {};
+
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -222,6 +236,51 @@ const Profile = () => {
           Sign Out
         </span>
       </div>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+
+      {userListings.length === 0 && (
+        <p className="text-slate-700 text-center font-bold mt-7">
+          No listings found
+        </p>
+      )}
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4">
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}>
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase">
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
